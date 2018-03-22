d46629fbaa99b5e3a45bcd7f154b6e20bdb517ee
kechuangsinosig <37279934+kechuangsinosig@users.noreply.github.com>
2018-03-22 09:20:37 +0800

    Create sinosig_config.java

---
+++ sinosig_config.java
@@ -0,0 +1,9 @@
+public class sinosig
+{
+  public static void main<String args[]>
+  {
+      String username = "sinosig";
+      String password = "config"
+      System.out.println(username,password);
+  }
+}
