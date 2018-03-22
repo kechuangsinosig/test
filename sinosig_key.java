32a9a5359af5d2cc9ba8af0dcf3610989fdb3fe1
kechuangsinosig <37279934+kechuangsinosig@users.noreply.github.com>
2018-03-22 09:18:43 +0800

    Create sinosig_key.java

---
+++ sinosig_key.java
@@ -0,0 +1,9 @@
+public class sinosig
+{
+  public static void main<String args[]>
+  {
+      String username = "sinosig";
+      String password = "key"
+      System.out.println(username,password);
+  }
+}
